f313c8f5501e99d2b216b2ffe9570d1e07b58410
JayeUP <40747954+JayeUP@users.noreply.github.com>
2019-06-04 21:48:52 +0800

    提交人：金元 内容：修改配置

--- guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
+++ guns-film/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
@@ -29,13 +29,13 @@ public class EntityGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("D:\\tmp\\guns\\guns-rest\\src\\main\\java");//这里写你自己的java目录
+        gc.setOutputDir("C:\\Users\\82005\\IdeaProjects\\meetingfilm\\guns\\guns-film\\src\\main\\java");//这里写你自己的java目录
         gc.setFileOverride(true);//是否覆盖
         gc.setActiveRecord(true);
         gc.setEnableCache(false);// XML 二级缓存
         gc.setBaseResultMap(true);// XML ResultMap
         gc.setBaseColumnList(false);// XML columList
-        gc.setAuthor("stylefeng");
+        gc.setAuthor("jy"); //修改作者
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -48,10 +48,10 @@ public class EntityGenerator {
                 return super.processTypeConvert(fieldType);
             }
         });
-        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
-        dsc.setPassword("root");
-        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/rest?characterEncoding=utf8");
+        dsc.setPassword("123456");
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/guns_rest?serverTimezone=GMT&characterEncoding=utf8");
         mpg.setDataSource(dsc);
 
         // 策略配置
@@ -64,11 +64,11 @@ public class EntityGenerator {
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setParent(null);
-        pc.setEntity("com.stylefeng.guns.rest.persistence.model");
-        pc.setMapper("com.stylefeng.guns.rest.persistence.dao");
-        pc.setXml("com.stylefeng.guns.rest.persistence.dao.mapping");
-        pc.setService("TTT");       //本项目没用，生成之后删掉
-        pc.setServiceImpl("TTT");   //本项目没用，生成之后删掉
+        pc.setEntity("com.stylefeng.guns.rest.common.persistence.model");
+        pc.setMapper("com.stylefeng.guns.rest.common.persistence.dao");
+        pc.setXml("com.stylefeng.guns.rest.common.persistence.dao.mapping");
+        pc.setService("TTT");
+        pc.setServiceImpl("TTT");
         pc.setController("TTT");    //本项目没用，生成之后删掉
         mpg.setPackageInfo(pc);
 
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -3,9 +3,11 @@ package com.stylefeng.guns.rest.modular.film;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.stylefeng.guns.rest.film.FilmService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/film")
 public class FilmController {
 
     @RequestMapping("/test")
@@ -20,5 +22,11 @@ public class FilmController {
     @RequestMapping("/testFilm")
     public String testFilm(){
         return filmService.test();
+    }
+
+    //首页请求
+    @RequestMapping(value = "/getIndex",method = RequestMethod.GET)
+    public String getIndex(){
+        return null;
     }
 }
