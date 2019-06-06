ce0ade54cee1516a7b81a9765e32e0171f4478e1
Nemo <670154728@qq.com>
2019-06-06 16:58:28 +0800

    user-ready-to-work

---
+++ guns-api/src/main/java/com/stylefeng/guns/rest/user/FilmUserService.java
@@ -0,0 +1,9 @@
+package com.stylefeng.guns.rest.user;
+
+/**
+ * @author Nemo
+ * @version 1.0
+ * @date 2019/6/6
+ */
+public interface FilmUserService {
+}
---
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/service/impl/FilmUserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.stylefeng.guns.rest.modular.user.service.impl;
+
+import com.stylefeng.guns.rest.user.FilmUserService;
+
+/**
+ * @author Nemo
+ * @version 1.0
+ * @date 2019/6/6
+ */
+public class FilmUserServiceImpl implements FilmUserService {
+}
