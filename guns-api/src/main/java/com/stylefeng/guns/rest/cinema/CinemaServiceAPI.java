b2914518de783d27edbd3d9004727cefc35f84c5
SandSEA <1090318483@qq.com>
2019-06-11 22:07:18 +0800

    更新cinema

--- guns-api/src/main/java/com/stylefeng/guns/rest/cinema/CinemaServiceAPI.java
+++ guns-api/src/main/java/com/stylefeng/guns/rest/cinema/CinemaServiceAPI.java
@@ -1,8 +1,7 @@
 package com.stylefeng.guns.rest.cinema;
 
-import com.stylefeng.guns.rest.cinema.vo.CinemaInfoVO;
-import com.stylefeng.guns.rest.cinema.vo.FilmInfoVO;
-import com.stylefeng.guns.rest.cinema.vo.HallInfoVO;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.stylefeng.guns.rest.cinema.vo.*;
 
 import java.util.List;
 
@@ -11,6 +10,38 @@ import java.util.List;
  * @Data 2019/6/4 22:16
  */
 public interface CinemaServiceAPI {
+
+
+    /***
+     * 1.根据CinemaQueryVO, 查询影院列表
+     * @param cinemaQueryVO 查询条件
+     * @return Page<CinemaVO>
+     */
+    Page<CinemaVO> getCinemas(CinemaQueryVO cinemaQueryVO);
+
+
+    /***
+     * 2.根据条件获取品牌列表
+     * @param brandId 影院编号
+     * @return List<BrandVO>
+     */
+    List<BrandVO> getBrands(int brandId);
+
+
+    /***
+     * 3.获取行政区域列表
+     * @param areaId 行政区
+     * @return List<AreaVO>
+     */
+    List<AreaVO> getAreas(int areaId);
+
+
+    /***
+     * 4.获取影厅类型列表
+     * @param hallType 影厅类型
+     * @return List<HallTypeVO>
+     */
+    List<HallTypeVO> getHallTypes(int hallType);
 
     /**
      * 根据影院编号，获取影院信息
---
+++ guns-api/src/main/java/com/stylefeng/guns/rest/cinema/vo/HallTypeVO.java
@@ -0,0 +1,14 @@
+package com.stylefeng.guns.rest.cinema.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class HallTypeVO implements Serializable {
+
+    private String halltypeId;
+    private String halltypeName;
+    private boolean isActive;
+
+}
